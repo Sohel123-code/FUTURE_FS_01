0e586485dec3ea3e499fefa612f9d8c44e2d9d8c
Mohamed khaja eshaq <mdsohel46940@gmail.com>
2026-06-26 20:35:55 +0530

    Beyond degrees

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,6 +11,8 @@ import otpImg from '../assets/otp generator.webp';
 import smsPredictorImg from '../assets/sms_predictor.png';
 import cleanSlateImg from '../assets/cleanslate.png';
 import ecosphereImg from '../assets/ecosphere.jpg';
+import beyondDegreesImg from '../assets/Screenshot 2026-06-26 202042.png';
+
 
 function Projects() {
   const basicProjects = [
@@ -104,8 +106,8 @@ function Projects() {
     {
       title: "BEYOND DEGREES",
       desc: "A platform focused on skill-building and continuous learning beyond traditional education.",
-      img: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?auto=format&fit=crop&w=1000&q=80",
-      link: "https://beyond-degrees2-sohel123-codes-projects.vercel.app/"
+      img: beyondDegreesImg,
+      link: "https://beyond-degrees2-3meag9zig-sohel123-codes-projects.vercel.app/"
     }
   ];
 
